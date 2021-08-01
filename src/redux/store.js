729207d9590c066170b365a5332d554a14f2b3f9
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2021-07-31 22:55:10 -0700

    logger on dev build only

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,12 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+//set logger to development build only
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
